d57562ae7a9f6dbf9658787b42f13d9273c15f17
Alejandroesbr <alejandroescobare@outlook.com>
2026-06-01 15:49:18 -0500

    funciones: shared dom.js

--- src/shared/dom.js
+++ src/shared/dom.js
@@ -1,1 +1,15 @@
 // qs(), render(), createElement()
+
+export function qs(selector) {
+  return document.querySelector(selector);
+}
+
+export function render(selector, html) {
+  document.querySelector(selector).innerHTML = html;
+}
+
+export function createElement(tag, text = "") {
+  const element = document.createElement(tag);
+  element.textContent = text;
+  return element;
+}
